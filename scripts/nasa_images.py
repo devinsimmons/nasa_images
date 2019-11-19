e4dda0f477d04219780f0ff1cb03bccf7be62fe2
Devin Simmons <39880645+devinsimmons@users.noreply.github.com>
2019-11-19 16:38:55 -0500

    fixed error

--- scripts/nasa_images.py
+++ scripts/nasa_images.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import urllib
 import os.path 
+import json
 
 site = 'https://earthobservatory.nasa.gov/images'
 
@@ -23,8 +24,26 @@ for img in imgs:
         urllib.request.urlretrieve(map_img.a.get('href'), filename)
                                 
 #scrolling further through the webpage to get more images
-for i in range(2, 3):
+for i in range(3, 30):
     site = 'https://earthobservatory.nasa.gov/topic/image-of-the-day/getRecords?page=' + str(i)
-    page = urllib.request.urlopen(site).read()
-    soup = bs(page, features= "html.parser")
-    print(soup.data)
+    page = urllib.request.urlopen(site)
+    data = json.load(page)
+    
+    #looping through dictionary entry, which contains details that let me
+    #load each post
+    for i in data['data']:
+        slug = i['slug'].split('/')[-1]
+        link = 'https://earthobservatory.nasa.gov/images/' + str(i['id']) + '/' + slug
+        
+        print(link)
+        page = urllib.request.urlopen(link)
+        
+        soup = bs(page, features = 'html.parser')
+        soup.prettify()
+        
+        map_img = soup.findAll('div', {'class': 'panel-image'})[0]
+        if map_img.a:
+            filename = r"C:\Users\devin.simmons.ctr\Desktop\projects\nasa_images\images\\" + map_img.a.get('href').split('/')[-1]
+        
+        if not os.path.exists(filename):
+            urllib.request.urlretrieve(map_img.a.get('href'), filename)
